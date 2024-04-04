d5da65c4eedf53d68fb78094b7b80f165648a7d0
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2024-04-04 11:42:19 +0200

    reset fix and add a variable 'currentLevel'

--- src/background.hpp
+++ src/background.hpp
@@ -1,10 +1,10 @@
 #ifndef BACKGROUND_HPP
 #define BACKGROUND_HPP
 
+int currentLevel=0;
 
 class Background
 {
-    int n=0;
     int background_transformation_step = 0;
     int counter = BACKGROUND_CHANGING_REGULARITY;
     int increaser = 0;
@@ -21,7 +21,7 @@ class Background
             images[i].loadFromFile(picture[i]);
         }
         
-        sf::Vector2u textureSize = images[n].getSize();
+        sf::Vector2u textureSize = images[currentLevel].getSize();
         sf::Vector2u windowSize = window.getSize();
         background1.setScale((float)windowSize.x / textureSize.x, (float)windowSize.y / textureSize.y);
         background2.setScale((float)windowSize.x / textureSize.x, (float)windowSize.y / textureSize.y);
@@ -30,6 +30,15 @@ class Background
         background2.setPosition(window.getSize().x, 0);
         background1.setPosition(0, 0);
         drawIt(window);
+    }
+    void reset(sf::RenderWindow& window) {
+        currentLevel = 0;
+        counter = BACKGROUND_CHANGING_REGULARITY;
+        increaser = 0;
+        background_transformation_step = 0;
+        setImage(window, 0);
+        background2.setPosition(window.getSize().x, 0);
+        background1.setPosition(0, 0);
     }
 
     void update(sf::RenderWindow& window) {
@@ -64,7 +73,7 @@ class Background
         if (counter == BACKGROUND_CHANGING_REGULARITY * speedFactor){ 
             counter = 0;
             if (background_transformation_step == 0){ 
-                n++; if (n>3) n=0;
+                currentLevel++; if (currentLevel>3) currentLevel=0;
                 background_transformation_step = 1; //change As Soon As Possible : when one picture perfectly fits the screen
                 //UPDATE SPEED
                 speedUp();
@@ -106,10 +115,10 @@ class Background
 
     void setImage(sf::RenderWindow& window, int background_number=0){ //0 = both, 1 = back 1 and 2 = back2
         if (background_number == 1 || background_number == 0) {
-            background1.setTexture(images[n]);
+            background1.setTexture(images[currentLevel]);
         }
         if (background_number == 2 || background_number == 0) {
-            background2.setTexture(images[n]);
+            background2.setTexture(images[currentLevel]);
         }
     }
 
--- src/definitions.hpp
+++ src/definitions.hpp
@@ -17,7 +17,7 @@
 #define INITIAL_SPEED 3 
 #define FINAL_SPEED 20
 #define SPEED_INCREASE_INTERVAL 0 // skipped frames before increasing the speed by SPEED_INCREASE_VALUE (below)
-#define SPEED_INCREASE_VALUE 3.0f //HERE NORMAL = 1.0f, test = 3.0f
+#define SPEED_INCREASE_VALUE 1.0f //HERE NORMAL = 1.0f, test = 3.0f
 
 //---------- GRAVITY ---------
 #define PROPULSION_STRENGHT 2.1f
@@ -44,11 +44,9 @@
 
 //----------- SFML -----------
 #include <string.h>
-//#include <unistd.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-//#include <SFML/Audio.hpp>
 #include <vector>
 
 using namespace sf;
@@ -61,7 +59,6 @@ using namespace std;
 #include "score.hpp"
 #include "gameOver.hpp"
 #include "animation.hpp"
-//#include "sound.hpp"
 
 //---------- WINDOW ----------
 sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, COLOR_DEPTH), "SFML works!");
