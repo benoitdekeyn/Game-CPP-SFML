d3aa0f4789be687117e4765295ad70460c9af94f
Benoît de Keyn <146000855+benoitdekeyn-algosup@users.noreply.github.com>
2024-04-04 14:43:15 +0200

    enhance music

--- src/music.hpp
+++ src/music.hpp
@@ -7,11 +7,13 @@ class GameMusic
     sf::Music music;
     std::string BG_musics[4] = {"../Assets/Musics/Hinox_Battle_BOTW_bg1.mp3", "../Assets/Musics/Dark_Owl_bg2.mp3", "../Assets/Musics/Lost_Woods_BOTW_bg3.mp3", "../Assets/Musics/SHadow_Forest_bg4.mp3"};
     std::string menu_music = "../Assets/Musics/Endless_sand.mp3";
+
 public:
     GameMusic()
     {
         music.openFromFile(BG_musics[currentLevel]);
         music.setLoop(true);
+        playMenu();
     }
     
     void update(){
@@ -25,7 +27,7 @@ public:
     }
 
     void playMenu(){
-        music.stop();
+        stop();
         music.openFromFile(menu_music);
         music.play();
     }
